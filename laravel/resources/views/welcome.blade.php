7dbee712f671bb3df1bbc6651ff4a38f1231878e
haoranyu <haoran_yu@hotmail.com>
2015-03-30 16:19:09 -0500

    Modify form and allow search

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -4,17 +4,17 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="jumbotron text-center">
+            <form class="jumbotron text-center" action="/s">
                 <h1>Researchew</h1>
                 <p class="text-muted">Find some research interest you have here...</p>
                 <div class="input-group input-group-lg">
 
-                    <input type="text" class="form-control" placeholder="Type in the keyword">
+                    <input type="text" name="q" class="form-control" placeholder="Type in the keyword">
                     <span class="input-group-btn">
-                        <a class="btn btn-primary"> Search </a>
+                        <button type="submit" class="btn btn-primary"> Search </button>
                     </span>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
