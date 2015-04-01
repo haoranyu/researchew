f4c6064ce62f88871902eec31e32d5968031f9fb
haoranyu <haoran_yu@hotmail.com>
2015-04-01 02:44:21 -0500

    Reform the homepage style

--- laravel/app/views/home.blade.php
+++ laravel/app/views/home.blade.php
@@ -1,1 +1,22 @@
 @extends('theme/main')
+@section('content')
+<div class="am-container">
+    <div class="search-box">
+        <h1>Researchew</h1>
+        <p>Find some research interest you have here...</p>
+        <div class="am-input-group am-input-group-lg">
+            <input type="text" id="query" class="am-form-field" placeholder="Type in the keyword">
+            <span class="am-input-group-btn">
+                <a class="am-btn am-btn-primary" id="search"> Search </a>
+            </span>
+        </div>
+    </div>
+</div>
+@endsection
+@section('script')
+<script>
+    $(document).on('click', '#search', function(){
+        location.href = "/search/" + $('#query').val() + "/";
+    });
+</script>
+@endsection
